a76ff1f3697f9863559063d0634c257d66f2b9e3
Daniel Frey <daniel.stephen.frey@gmail.com>
2022-09-22 23:13:10 +0000

    Add logger

--- src/server.js
+++ src/server.js
@@ -1,12 +1,14 @@
 'use strict';
 
 const express = require('express');
+const logger = require('./middleware/logger');
 const cymbalRouter = require('./routes/cymbals');
 const stickRouter = require('./routes/sticks');
 const PORT = process.env.PORT || 3002;
 
 const app = express();
 app.use(express.json());
+app.use(logger);
 app.use(cymbalRouter);
 app.use(stickRouter);
 
