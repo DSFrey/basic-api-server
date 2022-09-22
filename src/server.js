11011d5fdd46f62ad1d4c49c7379a230e8897ecc
Daniel Frey <daniel.stephen.frey@gmail.com>
2022-09-22 23:34:52 +0000

    Add error handling

--- src/server.js
+++ src/server.js
@@ -1,6 +1,8 @@
 'use strict';
 
 const express = require('express');
+const notFound = require('./error-handlers/404');
+const errorHandler = require('./error-handlers/500');
 const logger = require('./middleware/logger');
 const cymbalRouter = require('./routes/cymbals');
 const stickRouter = require('./routes/sticks');
@@ -9,8 +11,12 @@ const PORT = process.env.PORT || 3002;
 const app = express();
 app.use(express.json());
 app.use(logger);
+
 app.use(cymbalRouter);
 app.use(stickRouter);
+
+app.use('*', notFound);
+app.use(errorHandler);
 
 function start(){
   app.listen(PORT, () => console.log('listening on port', PORT));
