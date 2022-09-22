a6d6301e9da20f4478d2e52d4986c9a84c4a77d9
Daniel Frey <daniel.stephen.frey@gmail.com>
2022-09-22 22:38:00 +0000

    Add the rest of the CRUD functions for both cymbals and sticks

--- src/routes/cymbals.js
+++ src/routes/cymbals.js
@@ -10,4 +10,29 @@ router.post('/cymbal', async (req, res, send) => {
   res.status(200).send(newCymbal);
 });
 
+router.get('/cymbal', async (req, res, send) => {
+  const allCymbals = await CymbalModel.findAll();
+  res.status(200).send(allCymbals);
+});
+
+router.get('/cymbal/:id', async (req, res, send) => {
+  const id = req.params.id;
+  const oneCymbal = await CymbalModel.findAll({ where: { id } });
+  res.status(200).send(oneCymbal);
+});
+
+router.put('/cymbal/:id', async (req, res, send) => {
+  const id = req.params.id;
+  await CymbalModel.update(req.body, { where: { id } });
+  const oneCymbal = await CymbalModel.findAll({ where: { id } });
+  res.status(200).send(oneCymbal);
+});
+
+router.delete('/cymbal/:id', async (req, res, send) => {
+  const id = req.params.id;
+  await CymbalModel.destroy({ where: { id } });
+  const oneCymbal = await CymbalModel.findAll({ where: { id } });
+  res.status(200).send(oneCymbal);
+});
+
 module.exports = router;
--- src/routes/sticks.js
+++ src/routes/sticks.js
@@ -10,4 +10,29 @@ router.post('/stick', async (req, res, send) => {
   res.status(200).send(newStick);
 });
 
+router.get('/stick', async (req, res, send) => {
+  const allSticks = await StickModel.findAll();
+  res.status(200).send(allSticks);
+});
+
+router.get('/stick/:id', async (req, res, send) => {
+  const id = req.params.id;
+  const oneStick = await StickModel.findAll({ where: { id } });
+  res.status(200).send(oneStick);
+});
+
+router.put('/stick/:id', async (req, res, send) => {
+  const id = req.params.id;
+  await StickModel.update(req.body, { where: { id } });
+  const oneStick = await StickModel.findAll({ where: { id } });
+  res.status(200).send(oneStick);
+});
+
+router.delete('/stick/:id', async (req, res, send) => {
+  const id = req.params.id;
+  await StickModel.destroy({ where: { id } });
+  const oneStick = await StickModel.findAll({ where: { id } });
+  res.status(200).send(oneStick);
+});
+
 module.exports = router;
